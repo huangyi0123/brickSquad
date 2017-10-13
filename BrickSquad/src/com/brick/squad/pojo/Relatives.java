5a162afc8984f40fe2b399f9b6b6b1ffe4cdf756
liangchengyue <2524499314@qq.com>
2017-10-13 15:36:36 +0800

    添加pojo

---
+++ BrickSquad/src/com/brick/squad/pojo/Relatives.java
@@ -0,0 +1,51 @@
+package com.brick.squad.pojo;
+/**
+ * 老人亲属关系
+ * @author 梁城月
+ *
+ */
+public class Relatives {
+	private String id;
+	private String perId;
+	private String name;
+	private String telephone;
+	private String addressId;
+	private String relationshipId;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPerId() {
+		return perId;
+	}
+	public void setPerId(String perId) {
+		this.perId = perId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	public String getAddressId() {
+		return addressId;
+	}
+	public void setAddressId(String addressId) {
+		this.addressId = addressId;
+	}
+	public String getRelationshipId() {
+		return relationshipId;
+	}
+	public void setRelationshipId(String relationshipId) {
+		this.relationshipId = relationshipId;
+	}
+	
+}
