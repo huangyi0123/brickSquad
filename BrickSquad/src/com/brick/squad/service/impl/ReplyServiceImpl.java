1bac2be82c957422d51bd1449743eb4c50cd4894
吴文鑫 <1667375996@qq.com>
2017-10-13 19:57:56 +0800

    回复表的增删查改

---
+++ BrickSquad/src/com/brick/squad/service/impl/ReplyServiceImpl.java
@@ -0,0 +1,37 @@
+package com.brick.squad.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import com.brick.squad.mapper.ReplyMapper;
+import com.brick.squad.pojo.Reply;
+
+public class ReplyServiceImpl implements ReplyService {
+	@Autowired
+	@Qualifier("replyMapper")
+	private ReplyMapper replyMapper;
+
+	public Reply findReplyById(String id) {
+
+		return replyMapper.findReplyById(id);
+	}
+
+	@Override
+	public void insertReply(Reply reply) {
+		replyMapper.insertReply(reply);
+
+	}
+
+	@Override
+	public void deleteReplyById(String id) {
+		replyMapper.deleteReplyById(id);
+
+	}
+
+	@Override
+	public void updateReplyCententById(Reply reply) {
+		replyMapper.updateReplyCententById(reply);
+
+	}
+
+}
