843570a853351a00c634655e5de94ef88f589039
yxb1994 <1032349486@qq.com>
2017-10-16 21:04:17 +0800

    订单项的增删改查

---
+++ BrickSquad/src/com/brick/squad/pojo/OrderDetails.java
@@ -0,0 +1,53 @@
+package com.brick.squad.pojo;
+
+public class OrderDetails {
+    private String id;
+
+    private String articleId;
+
+    private String ordersId;
+
+    private Integer number;
+
+    private Float subtotal;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getArticleId() {
+        return articleId;
+    }
+
+    public void setArticleId(String articleId) {
+        this.articleId = articleId;
+    }
+
+    public String getOrdersId() {
+        return ordersId;
+    }
+
+    public void setOrdersId(String ordersId) {
+        this.ordersId = ordersId;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Float getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(Float subtotal) {
+        this.subtotal = subtotal;
+    }
+}
