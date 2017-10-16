643719c7d7cdd336529874fd78f43d2132082fe1
吴文鑫 <1667375996@qq.com>
2017-10-16 16:59:20 +0800

    ss

--- BrickSquad/src/com/brick/squad/test/ActivitiesServiceTest.java
+++ BrickSquad/src/com/brick/squad/test/ActivitiesServiceTest.java
@@ -17,9 +17,7 @@ import com.brick.squad.util.JunitClassRunner;
 @RunWith(JunitClassRunner.class)
 @ContextConfiguration(locations="classpath:com/brick/squad/config/applicationContext.xml")
 public class ActivitiesServiceTest {
-	/**
-	 * woshinige
-	 */
+	
 	@Autowired
 	@Qualifier("activitiesService")
 	private ActivitiesService activitiesService;
