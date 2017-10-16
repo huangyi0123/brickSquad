2ff420307381803908623af63da393f07b7cc136
yxb1994 <1032349486@qq.com>
2017-10-16 08:55:24 +0800

    修改Orders订单，ShoppingCar购物车，Rapport客户老人沟通与其他的表的关系得映射

--- BrickSquad/src/com/brick/squad/pojo/Rapport.java
+++ BrickSquad/src/com/brick/squad/pojo/Rapport.java
@@ -13,15 +13,17 @@ public class Rapport {
     private Date rapportDate;
 
     private String content;
-    /**Rapport客户老人沟通与User用户的关系：多对多*/
-    private List<User> users;
+    /**Rapport客户老人沟通与User用户的关系：一对多*/
+    private User users;
     /**Rapport客户老人沟通与老人的关系：一对多*/
     private PersonalInformation personalInformation;
-	public List<User> getUsers() {
+
+
+	public User getUsers() {
 		return users;
 	}
 
-	public void setUsers(List<User> users) {
+	public void setUsers(User users) {
 		this.users = users;
 	}
 
