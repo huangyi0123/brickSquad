f421fcdf4699f86e2d88c37ad3f7d6ce17539b20
zrk86408460 <906031318@qq.com>
2017-10-18 15:33:08 +0800

    修改user表的插入方法

--- BrickSquad/src/com/brick/squad/service/UserService.java
+++ BrickSquad/src/com/brick/squad/service/UserService.java
@@ -6,10 +6,11 @@ import com.brick.squad.util.Pagination;
 
 public interface UserService {
 	public List<User> findUsername(String username);
-	public void addUser(String id,String username,String password,String telephone,String roleId);
+	/*public void addUser(String id,String username,String password,String telephone,String roleId);*/
 	public void deleteUser(String username);
 	public void updateUser(String username,String password);
 	public String userPagination(Pagination pagination);
+	public void addUser(User user);
 	//用户分页显示
 		public int userCount(); 
 }
