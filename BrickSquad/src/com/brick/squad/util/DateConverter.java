1de2d1a302e9ea536a9fbe9847eaade30f4b934a
yxb1994 <1032349486@qq.com>
2017-10-19 19:49:30 +0800

    时间日期类型的转换

---
+++ BrickSquad/src/com/brick/squad/util/DateConverter.java
@@ -0,0 +1,22 @@
+package com.brick.squad.util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.core.convert.converter.Converter;
+
+public class DateConverter implements Converter<String, Date>{
+	SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	@Override
+	public Date convert(String source) {
+		
+		try {
+			return simpleDateFormat.parse(source);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
