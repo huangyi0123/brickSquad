29fba25d98590f30f097e8dd5a7ce56763793986
zrk86408460 <906031318@qq.com>
2017-10-21 09:25:31 +0800

    上传本地

--- BrickSquad/src/com/brick/squad/controller/RegionController.java
+++ BrickSquad/src/com/brick/squad/controller/RegionController.java
@@ -50,4 +50,10 @@ public class RegionController {
 		regionService.insertRegionById(region);
 		return "backstage_managed/jsp/region/region_list";
 	}
+	@RequestMapping(value="/deleteRegionById")
+	@ResponseBody
+	public String deleteRegionById(String id){
+		regionService.deleteRegionById(id);
+		return "success";
+	}
 }
