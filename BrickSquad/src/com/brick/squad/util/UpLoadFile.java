2b7974c63c9f6c3f82094b761c6c526667e40198
liangchengyue <2524499314@qq.com>
2017-10-26 11:21:44 +0800

    新闻内容上传（含图片）

---
+++ BrickSquad/src/com/brick/squad/util/UpLoadFile.java
@@ -0,0 +1,28 @@
+package com.brick.squad.util;
+
+import java.util.List;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+public class UpLoadFile {
+	private int errno;
+	private List<String> data;
+	public int getErrno() {
+		return errno;
+	}
+	public void setErrno(int errno) {
+		this.errno = errno;
+	}
+	public List<String> getData() {
+		return data;
+	}
+	public void setData(List<String> data) {
+		this.data = data;
+	}
+	public String toJson() {
+		JSONObject jsonObject=JSONObject.fromObject(this);
+		return jsonObject.toString();
+	}
+	
+}
