389210f77ce8e43f648cdaaa0308af209c66a875
吴文鑫 <1667375996@qq.com>
2017-11-08 19:21:11 +0800

    解决冲突

--- BrickSquad/src/com/brick/squad/controller/ShoppingCarController.java
+++ BrickSquad/src/com/brick/squad/controller/ShoppingCarController.java
@@ -26,99 +26,116 @@ public class ShoppingCarController {
 	@Autowired
 	@Qualifier("shoppingCarService")
 	private ShoppingCarService shoppingCarService;
-	@Autowired
+	/*@Autowired
 	@Qualifier("articalService")
 	private ArticalService articalService;
+*/
 	@RequestMapping("/toShoppingCarList")
 	public String toRegionList() {
 		return "backstage_managed/jsp/shoppingcar/shoppingcar_list";
 	}
+
 	/**
 	 * 分页查询购物车信息
 	 * */
 	@RequestMapping("/getShoppingCarList")
 	@ResponseBody
-	public String getRegionList(int pSize,int cPage,String keyword) throws Exception {
-		Pagination pagination=new Pagination();
+	public String getRegionList(int pSize, int cPage, String keyword)
+			throws Exception {
+		Pagination pagination = new Pagination();
 		pagination.setKeyword(keyword);
 		System.out.println(keyword);
 		pagination.setCurentPage(cPage);
 		pagination.setPageSize(pSize);
-		String value=shoppingCarService.shoppingCarPagination(pagination);
-		return  value;
+		String value = shoppingCarService.shoppingCarPagination(pagination);
+		return value;
 	}
+
 	/**
 	 * 添加购物车信息跳转编辑页面
 	 * */
 	@RequestMapping("/toAddShoppingCar")
-	public String toAddShoppingCar(HttpServletRequest request,String id)throws Exception {
-		if(id!=null){
+	public String toAddShoppingCar(HttpServletRequest request, String id)
+			throws Exception {
+		if (id != null) {
 			request.setAttribute("msg", "修改");
 			request.setAttribute("url", "updateShoppingCar");
-			ShoppingCar shoppingCar= shoppingCarService.findShoppingCarById(id);
+			ShoppingCar shoppingCar = shoppingCarService
+					.findShoppingCarById(id);
 			request.setAttribute("shoppingCar", shoppingCar);
-		}else{
+		} else {
 			request.setAttribute("url", "insertShoppingCar");
 			request.setAttribute("msg", "添加");
 		}
 		return "backstage_managed/jsp/shoppingcar/add_shoppingcar";
 	}
+
 	/**
-	 * 下拉框查询的信息：
-	 * 查询老人姓名，商品名称
+	 * 下拉框查询的信息： 查询老人姓名，商品名称
 	 * */
 	@RequestMapping("/findArticleAndPersonalInformation")
 	@ResponseBody
-	public String findArticleAndPersonalInformation() throws Exception{
-		String articles=shoppingCarService.findArticleAndPersonalInformation();
+	public String findArticleAndPersonalInformation() throws Exception {
+		String articles = shoppingCarService
+				.findArticleAndPersonalInformation();
 		return articles;
 	}
+
 	/**
 	 * 设置时间转换格式
 	 * */
 	@InitBinder
 	protected void initBinder(WebDataBinder binder) {
-	    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-	    binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(
+				dateFormat, true));
 	}
+
 	/**
 	 * 插入购物车信息
 	 * */
 	@RequestMapping("/insertShoppingCar")
-	public String insertShoppingCar(ShoppingCar shoppingCar) throws Exception{
+	public String insertShoppingCar(ShoppingCar shoppingCar) throws Exception {
 		shoppingCarService.insertShoppingCar(shoppingCar);
 		return "backstage_managed/jsp/shoppingcar/shoppingcar_list";
 	}
+
 	/**
 	 * 删除购物车
 	 * */
 	@RequestMapping("deleteShoppingCar")
 	@ResponseBody
-	public String deleteShoppingCar(String id) throws Exception{
+	public String deleteShoppingCar(String id) throws Exception {
 		shoppingCarService.deleteShoppingCarById(id);
 		return "success";
 	}
+
 	/**
 	 * 修改购物车信息
 	 * */
 	@RequestMapping("/updateShoppingCar")
-	public String updateShoppingCar(ShoppingCar shoppingCar) throws Exception{
+	public String updateShoppingCar(ShoppingCar shoppingCar) throws Exception {
 		shoppingCarService.updateShoppingCarById(shoppingCar);
 		return "backstage_managed/jsp/shoppingcar/shoppingcar_list";
 	}
+
 	@RequestMapping("/findShoppingCarAndUserAndPsersonalInformationById")
-	public String findShoppingCarAndUserAndPsersonalInformationById(HttpServletRequest request,String id) throws Exception{
-		 ShoppingCarExpand shoppingCarExpand=shoppingCarService.findShoppingCarAndUserAndPsersonalInformationById(id);
-		 request.setAttribute("shoppingCarExpand", shoppingCarExpand);
+	public String findShoppingCarAndUserAndPsersonalInformationById(
+			HttpServletRequest request, String id) throws Exception {
+		ShoppingCarExpand shoppingCarExpand = shoppingCarService
+				.findShoppingCarAndUserAndPsersonalInformationById(id);
+		request.setAttribute("shoppingCarExpand", shoppingCarExpand);
 		return "backstage_managed/jsp/shoppingcar/search_shoppingcar";
 	}
+
 	/**
 	 * 前台跳转到购物车详情、准备数据页面
-	 * @throws Exception 
+	 * 
+	 * @throws Exception
 	 */
 	@RequestMapping("/detailsShoppingCar")
-	public String detailsShoppingCar(ShoppingCar shoppingCar) throws Exception{
-		String data =shoppingCarService.findArticIdAllArtic(shoppingCar);
+	public String detailsShoppingCar(ShoppingCar shoppingCar) throws Exception {
+		String data = shoppingCarService.findArticIdAllArtic(shoppingCar);
 		System.out.println(data);
 		return "frontEnd_manage/front_bootstrap/cart";
 	}
