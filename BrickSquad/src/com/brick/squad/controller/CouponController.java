34a5b19c064d6b026706581e58996234792cdc7c
LiangChengYue <2524499314@qq.com>
2017-11-13 13:44:11 +0800

    修改

--- BrickSquad/src/com/brick/squad/controller/CouponController.java
+++ BrickSquad/src/com/brick/squad/controller/CouponController.java
@@ -1,12 +1,26 @@
 package com.brick.squad.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.brick.squad.pojo.Type;
+import com.brick.squad.service.TypeService;
 @Controller
 @RequestMapping("/coupon")
 public class CouponController {
+	@Autowired
+	@Qualifier("typeService")
+	private TypeService typeService;
 	@RequestMapping("/findArticleType")
-	public String findArticleType() {
+	public String findArticleType(HttpServletRequest request) {
+		List<Type> types=typeService.getArctre("splb");
+		request.setAttribute("types", types);
 		return "frontEnd_manage/front_bootstrap/apply_coupon";
 	}
 }
