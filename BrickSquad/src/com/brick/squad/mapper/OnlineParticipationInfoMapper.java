5f30629313ac19c0ded180dfea2c745036ae3cb5
吴文鑫 <1667375996@qq.com>
2017-11-22 12:52:29 +0800

    在线参与表分页显示

--- BrickSquad/src/com/brick/squad/mapper/OnlineParticipationInfoMapper.java
+++ BrickSquad/src/com/brick/squad/mapper/OnlineParticipationInfoMapper.java
@@ -1,8 +1,14 @@
 package com.brick.squad.mapper;
 
+import java.util.List;
+
 import com.brick.squad.pojo.OnlineParticipationInfo;
+import com.brick.squad.util.Pagination;
 
 public interface OnlineParticipationInfoMapper {
 	public void insertOnlineParticipationInfo(
 			OnlineParticipationInfo onlineParticipationInfo);
+
+	public List<OnlineParticipationInfo> findOnlineParticipationInfoPagination(
+			Pagination pagination);
 }
