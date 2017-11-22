342cf76b731cad47623906664c64fd1012009313
吴文鑫 <1667375996@qq.com>
2017-11-22 10:21:48 +0800

    在线参与提交表单的数据js验证

--- BrickSquad/src/com/brick/squad/controller/OnlineParticipationInfoController.java
+++ BrickSquad/src/com/brick/squad/controller/OnlineParticipationInfoController.java
@@ -1,6 +1,8 @@
 package com.brick.squad.controller;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -31,13 +33,16 @@ public class OnlineParticipationInfoController {
 	@ResponseBody
 	public String userInsertOnlineParticipationInfo(
 			OnlineParticipationInfo onlineParticipationInfo) throws Exception {
+		String regex = "^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(17[013678])|(18[0,5-9]))\\d{8}$";
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(onlineParticipationInfo.getPhone());
+		boolean isMatch = m.matches();
 		if (onlineParticipationInfo.getUserName() == null
-				|| onlineParticipationInfo.getPhone() == null) {
+				|| onlineParticipationInfo.getPhone() == null || !(isMatch)) {
 			return "fail";
 		}
 		onlineParticipationInfoService
 				.insertOnlineParticipationInfo(onlineParticipationInfo);
-		System.out.println(onlineParticipationInfo.getId());
 		return "success";
 	}
 }
