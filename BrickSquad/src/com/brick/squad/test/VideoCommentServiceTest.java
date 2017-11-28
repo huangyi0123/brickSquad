532b1d699250d141352754aa6798317fcc3ccc2c
LiangChengYue <2524499314@qq.com>
2017-11-28 21:27:33 +0800

    修改

--- BrickSquad/src/com/brick/squad/test/VideoCommentServiceTest.java
+++ BrickSquad/src/com/brick/squad/test/VideoCommentServiceTest.java
@@ -1,11 +1,15 @@
 package com.brick.squad.test;
 
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 
+import com.brick.squad.pojo.Type;
 import com.brick.squad.pojo.VideoComment;
 import com.brick.squad.service.VideoCommentService;
 import com.brick.squad.util.JunitClassRunner;
@@ -24,5 +28,13 @@ public class VideoCommentServiceTest {
 		videoComment.setCommontUserId("7777");
 		videoComment.setContent("554545<Script>");
 		videoCommentService.insertVideoComment(videoComment);
+	}
+	@Test
+	public void testJSON(){
+		Type type=new Type();
+		type.setName("JSON");
+		String json="{\"chdren\":[],\"id\":\"\",\"name\":\"JSON\",\"parentId\":\"\"}";
+		JSONObject jsonObject=JSONObject.fromObject(json);
+		System.out.println(jsonObject.get("name"));
 	}
 }
